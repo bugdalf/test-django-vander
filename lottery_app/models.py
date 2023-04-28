3f53eccb1849225fb2a2b8aabf2e1048223409c4
Vander Idme <bugdalf@gmail.com>
2023-04-28 08:46:57 -0500

    avances propios

--- lottery_app/models.py
+++ lottery_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Sorteo(models.Model):
+    nombre = models.CharField(max_length=200)
+    fecha_sorteo = models.DateField()
+    premio = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
